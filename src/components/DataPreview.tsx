85379bd03acae2d1921ab3e880048cfc007a3f2c
Anna Lisowska <lisowska.a@gmail.com>
2026-03-28 15:02:03 +0100

    update depend,fixed problem with scss

--- src/components/DataPreview.tsx
+++ src/components/DataPreview.tsx
@@ -1,37 +1,29 @@
-import { FormDataDTO } from 'models/FormDataDTO';
-import { FunctionComponent } from 'react';
+import { FormDataDTO } from "models/FormDataDTO";
+import { FunctionComponent } from "react";
 
 interface Props {
   data: Partial<FormDataDTO>;
 }
 
 const DataPreview: FunctionComponent<Props> = ({ data }) => {
-
   return (
-    <div id="form-data-preview" className={'bg-white rounded-6 p-12 w-[600px]'}>
-
+    <div id="form-data-preview" className={"bg-white rounded-6 p-12 w-[600px]"}>
       <div className="text-head-3 font-medium mb-8">
-        Form data preview
+        Form data preview newsletter
       </div>
-      <div className={'grid grid-cols-2 gap-6 w-full'}>
-        <span className={'font-medium'}>
-          Email:
-        </span>
-        <span>
-          {data.email}
-        </span>
-        <span className={'font-medium'}>
-          Clinic:
-        </span>
+      <div className={"grid grid-cols-2 gap-6 w-full"}>
+        <span className={"font-medium"}>Email:</span>
+        <span>{data.email}</span>
+        <span className={"font-medium"}>Clinic:</span>
         <ul>
-          {data.clinic?.map(x => <li className="mb-4 leading-none">{x}</li>)}
+          {data.clinic?.map((x) => (
+            <li key={x} className="mb-4 leading-none">
+              {x}
+            </li>
+          ))}
         </ul>
-        <span className={'font-medium'}>
-          Accepted terms
-        </span>
-        <span>
-          {data.acceptedTerms ? 'Yes' : 'No'}
-        </span>
+        <span className={"font-medium"}>Accepted terms</span>
+        <span>{data.acceptedTerms ? "Yes" : "No"}</span>
       </div>
     </div>
   );
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,9 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document';
-import React from 'react';
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
   render() {
-
     return (
-      <Html lang={"en"} >
+      <Html lang={"en"}>
         <Head>
           <meta name="description" content="My Business" />
         </Head>
